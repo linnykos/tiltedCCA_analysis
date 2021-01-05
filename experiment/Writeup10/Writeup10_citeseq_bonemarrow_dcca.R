366bf6c669d7bd3807285b5f91628a08888f7c81
Kevin Lin <klin1234@gmail.com>
2021-01-05 16:51:50 -0500

    Minor changes

--- experiment/Writeup10/Writeup10_citeseq_bonemarrow_dcca.R
+++ experiment/Writeup10/Writeup10_citeseq_bonemarrow_dcca.R
@@ -16,5 +16,7 @@ set.seed(10)
 K <- 8
 dcca_res <- dcca_factor(mat_1, mat_2, rank_1 = K, rank_2 = K, verbose = T) 
 
-source_code <- readLines("../multiomicCCA_analysis/experiment/Writep10/Writeup10_10x_pbmc_dcca.R")
+rm(list = c("mat_1", "mat_2", "bm", "pca_clr_adt", "pca_log_rna", "umap_clr_adt", "umap_log_rna"))
+
+source_code <- readLines("../multiomicCCA_analysis/experiment/Writeup10/Writeup10_citeseq_bonemarrow_dcca.R")
 save.image("../../out/Writeup10_10x_pbmc_dcca.RData")
