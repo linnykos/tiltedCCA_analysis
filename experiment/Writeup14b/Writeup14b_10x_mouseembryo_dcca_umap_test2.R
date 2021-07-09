44168eff05c22f869de1c3ae9b02d5cff93ee9ed
Kevin Lin <klin1234@gmail.com>
2021-07-09 01:24:43 -0400

    We've fixed the umap problem

--- experiment/Writeup14b/Writeup14b_10x_mouseembryo_dcca_umap_test2.R
+++ experiment/Writeup14b/Writeup14b_10x_mouseembryo_dcca_umap_test2.R
@@ -5,6 +5,7 @@ library(BSgenome.Hsapiens.UCSC.hg38); library(GenomeInfoDb)
 library(dplyr); library(ggplot2); library(multiomicCCA)
 
 load("../../../../out/Writeup14/Writeup14_10x_mouseembryo_preprocess.RData")
+
 date_of_run <- Sys.time(); session_info <- sessionInfo()
 
 mat_1 <- t(mbrain[["SCT"]]@scale.data)
@@ -71,144 +72,9 @@ dist_mat <- Matrix::sparseMatrix(i = i_vec, j = j_vec, x = x_vec)
 
 rownames(dist_mat) <- rownames(mat_1)
 colnames(dist_mat) <- rownames(mat_1)
-g_obj <- SeuratObject::as.Graph(dist_mat)
 
-# what if we use euclidean
-set.seed(10)
-seurat_umap2 <- Seurat::RunUMAP(g_obj, metric = "euclidean")
-mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = seurat_umap2@cell.embeddings, 
-                                                  key = "UMAP", assay = "RNA")
-plot1 <- Seurat::DimPlot(mbrain2, reduction = "asdf", label = TRUE,
-                         group.by = "label_Savercat",
-                         repel = TRUE, label.size = 2.5) + Seurat::NoLegend()
-ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14b/Writeup14b_umap_original_test2.png"),
-                plot1, device = "png", width = 6, height = 5, units = "in")
-
-# what if we use cosine
-set.seed(10)
-seurat_umap2 <- Seurat::RunUMAP(g_obj, metric = "cosine")
-mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = seurat_umap2@cell.embeddings, 
-                                                  key = "UMAP", assay = "RNA")
-plot1 <- Seurat::DimPlot(mbrain2, reduction = "asdf", label = TRUE,
-                         group.by = "label_Savercat",
-                         repel = TRUE, label.size = 2.5) + Seurat::NoLegend()
-ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14b/Writeup14b_umap_original_test3.png"),
-                plot1, device = "png", width = 6, height = 5, units = "in")
-
-
-############
-# don't remove the diagonal
-set.seed(10)
-rann_res <- RANN::nn2(obj$common, k = 30)
-rann_res$nn.idx <- rann_res$nn.idx[,-1]
-rann_res$nn.dist <- rann_res$nn.dist[,-1]
-rann_res$nn.idx <- lapply(1:nrow(rann_res$nn.idx), function(i){
-  rann_res$nn.idx[i,]
-})
-rann_res$nn.dist <- lapply(1:nrow(rann_res$nn.dist), function(i){
-  rann_res$nn.dist[i,]
-})
-j_vec <- unlist(rann_res$nn.idx)
-i_vec <- unlist(lapply(1:length(rann_res$nn.idx), function(i){
-  rep(i, length(rann_res$nn.idx[[i]]))
-}))
-x_vec <- unlist(rann_res$nn.dist)
-dist_mat <- Matrix::sparseMatrix(i = i_vec, j = j_vec, x = x_vec)
-rownames(dist_mat) <- rownames(mat_1)
-colnames(dist_mat) <- rownames(mat_1)
-g_obj <- SeuratObject::as.Graph(dist_mat)
-set.seed(42)
-reticulate::py_set_seed(42)
-seurat_umap2 <- Seurat:::RunUMAP.Graph(g_obj, metric = "euclidean", seed.use = 10L)
-mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = seurat_umap2@cell.embeddings, 
-                                                  key = "UMAP", assay = "RNA")
-plot1 <- Seurat::DimPlot(mbrain2, reduction = "asdf", label = TRUE,
-                         group.by = "label_Savercat",
-                         repel = TRUE, label.size = 2.5) + Seurat::NoLegend()
-ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14b/Writeup14b_umap_original_test4.png"),
-                plot1, device = "png", width = 6, height = 5, units = "in")
-set.seed(42)
-reticulate::py_set_seed(42)
-seurat_umap2 <- Seurat:::RunUMAP.Graph(g_obj, metric = "euclidean", seed.use = 10L)
-mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = seurat_umap2@cell.embeddings, 
-                                                  key = "UMAP", assay = "RNA")
-plot1 <- Seurat::DimPlot(mbrain2, reduction = "asdf", label = TRUE,
-                         group.by = "label_Savercat",
-                         repel = TRUE, label.size = 2.5) + Seurat::NoLegend()
-ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14b/Writeup14b_umap_original_test4b.png"),
-                plot1, device = "png", width = 6, height = 5, units = "in")
-
-#######################
-
-# what if we called reticulate ourselves?
-library(reticulate)
-reticulate::use_virtualenv("~/.virtualenvs")
-object <- g_obj
-np <- reticulate::import("numpy", delay_load = TRUE)
-sp <- reticulate::import("scipy", delay_load = TRUE)
-sklearn <- reticulate::import("sklearn", delay_load = TRUE)
-umap <- reticulate::import("umap", delay_load = TRUE)
-diag(x = object) <- 0
-data <- object
-object <- sp$sparse$coo_matrix(arg1 = object)
-umap.method = 'umap-learn'
-n.components = 2L
-metric = 'correlation'
-n.epochs = 0L
-learning.rate = 1
-min.dist = 0.3
-spread = 1
-repulsion.strength = 1
-negative.sample.rate = 5L
-a = NULL
-b = NULL
-uwot.sgd = FALSE
-seed.use = 42L
-metric.kwds = NULL
-verbose = TRUE
-reduction.key = 'UMAP_'
-ab.params <- umap$umap_$find_ab_params(spread = spread, min_dist = min.dist)
-a <- a %||% ab.params[[1]]
-b <- b %||% ab.params[[2]]
-n.epochs <- n.epochs %||% 0L
-#random.state <- sklearn$utils$check_random_state(seed = as.integer(x = 42))
-random.state <- np$random$RandomState(42L)
-umap.args <- list(
-  data = object,
-  graph = object,
-  n_components = n.components,
-  initial_alpha = learning.rate,
-  a = a,
-  b = b,
-  gamma = repulsion.strength,
-  negative_sample_rate = negative.sample.rate,
-  n_epochs = as.integer(x = n.epochs),
-  random_state = random.state,
-  init = "spectral",
-  metric = metric,
-  metric_kwds = metric.kwds,
-  verbose = verbose
-)
-umap.args <- c(umap.args, list(
-  densmap = FALSE,
-  densmap_kwds = NULL,
-  output_dens = FALSE
-))
-embeddings <- do.call(what = umap$umap_$simplicial_set_embedding, args = umap.args)
-if (length(x = embeddings) == 2) {
-  embeddings <- embeddings[[1]]
-}
-rownames(x = embeddings) <- colnames(x = data)
-colnames(x = embeddings) <- paste0("UMAP_", 1:n.components)
-# center the embeddings on zero
-embeddings <- scale(x = embeddings, scale = FALSE)
-umap <- CreateDimReducObject(
-  embeddings = embeddings,
-  key = reduction.key,
-  assay = NULL,
-  global = TRUE
-)
-mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = umap@cell.embeddings, 
+embedding <- custom_umap(dist_mat)
+mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = embedding, 
                                                   key = "UMAP", assay = "RNA")
 plot1 <- Seurat::DimPlot(mbrain2, reduction = "asdf", label = TRUE,
                          group.by = "label_Savercat",
@@ -216,77 +82,12 @@ plot1 <- Seurat::DimPlot(mbrain2, reduction = "asdf", label = TRUE,
 ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14b/Writeup14b_umap_original_test4.png"),
                 plot1, device = "png", width = 6, height = 5, units = "in")
 
-library(reticulate)
-object <- g_obj
-np <- reticulate::import("numpy", delay_load = TRUE)
-sp <- reticulate::import("scipy", delay_load = TRUE)
-sklearn <- reticulate::import("sklearn", delay_load = TRUE)
-umap <- reticulate::import("umap", delay_load = TRUE)
-diag(x = object) <- 0
-data <- object
-object <- sp$sparse$coo_matrix(arg1 = object)
-umap.method = 'umap-learn'
-n.components = 2L
-metric = 'correlation'
-n.epochs = 0L
-learning.rate = 1
-min.dist = 0.3
-spread = 1
-repulsion.strength = 1
-negative.sample.rate = 5L
-a = NULL
-b = NULL
-uwot.sgd = FALSE
-seed.use = 42L
-metric.kwds = NULL
-verbose = TRUE
-reduction.key = 'UMAP_'
-ab.params <- umap$umap_$find_ab_params(spread = spread, min_dist = min.dist)
-a <- a %||% ab.params[[1]]
-b <- b %||% ab.params[[2]]
-n.epochs <- n.epochs %||% 0L
-#random.state <- sklearn$utils$check_random_state(seed = as.integer(x = 42))
-random.state <- np$random$RandomState(42L)
-umap.args <- list(
-  data = object,
-  graph = object,
-  n_components = n.components,
-  initial_alpha = learning.rate,
-  a = a,
-  b = b,
-  gamma = repulsion.strength,
-  negative_sample_rate = negative.sample.rate,
-  n_epochs = as.integer(x = n.epochs),
-  random_state = random.state,
-  init = "spectral",
-  metric = metric,
-  metric_kwds = metric.kwds,
-  verbose = verbose
-)
-umap.args <- c(umap.args, list(
-  densmap = FALSE,
-  densmap_kwds = NULL,
-  output_dens = FALSE
-))
-embeddings <- do.call(what = umap$umap_$simplicial_set_embedding, args = umap.args)
-if (length(x = embeddings) == 2) {
-  embeddings <- embeddings[[1]]
-}
-rownames(x = embeddings) <- colnames(x = data)
-colnames(x = embeddings) <- paste0("UMAP_", 1:n.components)
-# center the embeddings on zero
-embeddings <- scale(x = embeddings, scale = FALSE)
-umap <- CreateDimReducObject(
-  embeddings = embeddings,
-  key = reduction.key,
-  assay = NULL,
-  global = TRUE
-)
-mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = umap@cell.embeddings, 
+
+embedding <- custom_umap(dist_mat)
+mbrain2[["asdf"]] <- Seurat::CreateDimReducObject(embedding = embedding, 
                                                   key = "UMAP", assay = "RNA")
 plot1 <- Seurat::DimPlot(mbrain2, reduction = "asdf", label = TRUE,
                          group.by = "label_Savercat",
                          repel = TRUE, label.size = 2.5) + Seurat::NoLegend()
 ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14b/Writeup14b_umap_original_test4b.png"),
                 plot1, device = "png", width = 6, height = 5, units = "in")
-
