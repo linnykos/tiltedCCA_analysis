dbcc8468cb17467cee6e6ebda48fe04aca063ee7
Kevin Lin <klin1234@gmail.com>
2021-07-11 00:54:23 -0400

    Adding more memory

--- experiment/Writeup14b/Writeup14b_10x_mouseembryo_dcca_laplacian_variablecalculations.sh
+++ experiment/Writeup14b/Writeup14b_10x_mouseembryo_dcca_laplacian_variablecalculations.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#$ -N bm_glmpca_nb
+#$ -N dcca_mouseembryo
 #$ -j y
 #$ -o ../../../../out/Writeup14b/qsub/
+#$ -l m_mem_free=12G
 
 Rscript --no-save Writeup14b_10x_mouseembryo_dcca_laplacian_variablecalculations.R
