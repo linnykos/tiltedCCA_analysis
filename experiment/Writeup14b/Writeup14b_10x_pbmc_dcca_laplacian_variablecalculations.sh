07bb4a0eedafebff54e9d3120c74eae1ee80552f
Kevin Lin <klin1234@gmail.com>
2021-07-16 16:07:24 -0400

    Upping the memory limit

--- experiment/Writeup14b/Writeup14b_10x_pbmc_dcca_laplacian_variablecalculations.sh
+++ experiment/Writeup14b/Writeup14b_10x_pbmc_dcca_laplacian_variablecalculations.sh
@@ -2,6 +2,6 @@
 #$ -N dcca_pbmc
 #$ -j y
 #$ -o ../../../../out/Writeup14b/qsub/
-#$ -l m_mem_free=30G
+#$ -l m_mem_free=50G
 
 Rscript --no-save Writeup14b_10x_pbmc_dcca_laplacian_variablecalculations.R
