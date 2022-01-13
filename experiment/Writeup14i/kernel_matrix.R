ad7c6662451da0969a12080d8a4d84239a233dec
linnykos <klin1234@gmail.com>
2022-01-12 23:25:46 -0500

    About to hack tilted-CCA to accept the meta-cell dim-red

--- experiment/Writeup14i/kernel_matrix.R
+++ experiment/Writeup14i/kernel_matrix.R
@@ -1,32 +1,59 @@
 form_kernel_matrix <- function(mat, K, 
                                metacell_clustering, 
                                clustering_hierarchy,
-                               similarity_type = c("euclidean", "kernel")){
-  stopifnot(length(similarity_type) == 1, similarity_type == "kernel") # other one is not coded yet
-  
+                               symmetrize_func = c("max", "min", "average"),
+                               verbose = T){
+
+  if(verbose) print("Computing low-dimensional embedding")
   svd_res <- multiomicCCA:::.svd_truncated(mat, K = K, symmetric = F, rescale = F, 
                                            mean_vec = T, sd_vec = F, K_full_rank = F)
   dimred <- multiomicCCA:::.mult_mat_vec(svd_res$u, svd_res$d)
   avg_mat <- .compute_average_mat(dimred, metacell_clustering)
+  l2_vec <- apply(avg_mat, 1, multiomicCCA:::.l2norm)
+  avg_mat <- multiomicCCA:::.mult_vec_mat(1/l2_vec, avg_mat)
+  
+  if(verbose) print("Computing distance matrix")
   dist_mat <- as.matrix(stats::dist(avg_mat, method = "euclidean"))
   
+  if(verbose) print("Computing bandwidth")
   bandwidth_vec <- .compute_bandwidth(dist_mat, clustering_hierarchy)
   kernel_mat <- matrix(NA, nrow = nrow(dist_mat), ncol = ncol(dist_mat))
   
+  if(verbose) print("Computing kernel matrix")
   for(i in 1:nrow(kernel_mat)){
     kernel_mat[i,] <- exp(-dist_mat[i,]/bandwidth_vec[i])
   }
   
-  kernel_mat <- (kernel_mat + t(kernel_mat))/2
+  if(verbose) print("Symmeterizing kernel matrix")
+  if (symmetrize_func[1] == "max"){
+    kernel_mat <- pmax(kernel_mat, t(kernel_mat))
+  } else if(symmetrize_func[1] == "min"){
+    kernel_mat <- pmin(kernel_mat, t(kernel_mat))
+  } else {
+    kernel_mat <- (kernel_mat + t(kernel_mat))/2
+  }
+  
   kernel_mat
 }
 
 compute_min_embedding <- function(kernel_mat_1, 
                                   kernel_mat_2,
-                                  K = min(100, nrow(kernel_mat_1))){
+                                  K = min(100, nrow(kernel_mat_1)),
+                                  entrywise_func = c("max", "min"),
+                                  verbose = T){
   stopifnot(all(dim(kernel_mat_1) == dim(kernel_mat_2)))
+  n <- nrow(kernel_mat_1)
   
-  kernel_mat <- pmax(kernel_mat_1, kernel_mat_2)
+  # entrywise-max
+  if(verbose) print("Computing entry-wise maximum")
+  if(entrywise_func[1] == "max"){
+    kernel_mat <- pmax(kernel_mat_1, kernel_mat_2)
+  } else {
+    kernel_mat <- pmin(kernel_mat_1, kernel_mat_2)
+  }
+  
+  # projecting to become valid kernel matrix
+  if(verbose) print("Projecting into space of PSDs")
   svd_res <- multiomicCCA:::.svd_truncated(kernel_mat, 
                                            K = K, 
                                            symmetric = F,
@@ -34,14 +61,28 @@ compute_min_embedding <- function(kernel_mat_1,
                                            mean_vec = T, 
                                            sd_vec = F, 
                                            K_full_rank = F)
-  
   if(any(svd_res$d <= 0)){
     idx <- which(svd_res$d <= 0)
     svd_res$u <- svd_res$u[,-idx,drop = F]
     svd_res$d <- svd_res$d[-idx]
   }
+  kernel_mat <- tcrossprod(multiomicCCA:::.mult_mat_vec(svd_res$u, svd_res$d), svd_res$u)
   
-  # [[note to self: should be consider the laplacian?]]
+  # geometric centering
+  if(verbose) print("Centering kernel matrix")
+  centering_mat <- matrix(-1/n, nrow = n, ncol = n)
+  diag(centering_mat) <- 1-1/n
+  kernel_mat <- centering_mat %*% kernel_mat %*% centering_mat
+  
+  # compute embedding
+  if(verbose) print("Computing final embedding")
+  svd_res <- multiomicCCA:::.svd_truncated(kernel_mat, 
+                                           K = K, 
+                                           symmetric = F,
+                                           rescale = F, 
+                                           mean_vec = T, 
+                                           sd_vec = F, 
+                                           K_full_rank = F)
   dimred <- multiomicCCA:::.mult_mat_vec(svd_res$u, svd_res$d)
 }
 
@@ -58,16 +99,19 @@ compute_min_embedding <- function(kernel_mat_1,
 .compute_bandwidth <- function(dist_mat, clustering_hierarchy){
   tmp <- unlist(clustering_hierarchy); tmp <- tmp[!is.na(tmp)]
   stopifnot(length(unique(tmp)) == length(tmp), 
-            all(tmp %% 1) == 0,
-            length(tmp) == nrow(dist_mat),
+            all(tmp %% 1 == 0),
+            all(sort(tmp) == 1:nrow(dist_mat)),
             nrow(dist_mat) == ncol(dist_mat))
   bandwidth_vec <- rep(NA, max(tmp))
   
-  for(idx in clustering_hierarchy){
-    if(all(is.na(idx))) next()
+  for(i in 1:nrow(dist_mat)){
+    k <- which(sapply(clustering_hierarchy, function(vec){i %in% vec}))
+    stopifnot(length(k) == 1)
     
-    max_val <- max(dist_mat[idx,idx])
-    bandwidth_vec[idx] <- max_val
+    bandwidth_vec[i] <- max(dist_mat[i, clustering_hierarchy[[k]]])
+    if(bandwidth_vec[i] == 0){
+      bandwidth_vec[i] <- min(dist_mat[i, -clustering_hierarchy[[k]]])/2
+    }
   }
   
   stopifnot(all(!is.na(bandwidth_vec)))
--- experiment/Writeup14i/metacell.R
+++ experiment/Writeup14i/metacell.R
@@ -1,10 +1,12 @@
-form_subclusters <- function(mat, clustering, target_k){
+form_subclusters <- function(mat, clustering, target_k, 
+                             verbose = T){
   df <- .compute_subcluster_splits(clustering = clustering, 
-                                   mat = mat, 
+                                   n = nrow(mat), 
                                    target_k = target_k)
   
   tmp <- lapply(1:length(clustering), function(k){
-    .compute_metacells(k = df$size[k], 
+    if(verbose) print(paste0("On cluster ", k))
+    .compute_metacells(k = df$num[k], 
                        mat = mat[clustering[[k]],,drop = F],
                        row_indices = clustering[[k]])
   })
@@ -14,6 +16,7 @@ form_subclusters <- function(mat, clustering, target_k){
 
 intersect_metacells <- function(metacell_clustering_1, 
                                 metacell_clustering_2,
+                                min_size = 5,
                                 n){
   stopifnot(n >= max(unlist(metacell_clustering_1)),
             n >= max(unlist(metacell_clustering_2)))
@@ -44,16 +47,20 @@ intersect_metacells <- function(metacell_clustering_1,
   metacell_clustering <- lapply(uniq_vec, function(i){
     which(total_idx == i)
   })
+  names(metacell_clustering) <- as.character(uniq_vec)
+  metacell_clustering <- metacell_clustering[sapply(metacell_clustering, length) >= min_size]
+  uniq_vec <- as.numeric(names(metacell_clustering))
+  
   clustering_hierarchy_1 <- sapply(1:total_1, function(i){
     idx <- which(idx_df$modality_1 == i)
     if(length(idx) > 0){
-      idx_df$total_idx[idx]
+      which(uniq_vec %in% idx_df$total_idx[idx])
     } else NA
   })
   clustering_hierarchy_2 <- sapply(1:total_2, function(i){
     idx <- which(idx_df$modality_2 == i)
     if(length(idx) > 0){
-      idx_df$total_idx[idx]
+      which(uniq_vec %in% idx_df$total_idx[idx])
     } else NA
   })
   
@@ -65,19 +72,19 @@ intersect_metacells <- function(metacell_clustering_1,
 
 #######################
 
-.compute_subcluster_splits <- function(clustering, mat, target_k){
+.compute_subcluster_splits <- function(clustering, n, target_k){
   stopifnot(is.list(clustering), 
-            sum(sapply(clustering, length)) <= nrow(mat),
+            sum(sapply(clustering, length)) <= n,
             length(clustering) <= target_k,
             target_k > 0, target_k %% 1 == 0)
   tmp <- unlist(clustering)
-  stopifnot(all(tmp %% 1) == 0, table(tmp) == 1, all(tmp > 0))
+  stopifnot(all(tmp %% 1 == 0), table(tmp) == 1, all(tmp > 0))
   
   df <- data.frame(total_size = sapply(clustering, length),
                    size = sapply(clustering, length), 
                    num = rep(1, length(clustering)))
   if(all(is.null(names(clustering)))){
-    rownames(df) <- 1:nrow(clustering)
+    rownames(df) <- 1:length(clustering)
   } else {
     rownames(df) <- names(clustering)
   }
@@ -94,8 +101,12 @@ intersect_metacells <- function(metacell_clustering_1,
 .compute_metacells <- function(k, mat, row_indices){
   stopifnot(length(row_indices) == nrow(mat))
   
-  svd_res <- multiomicCCA:::.svd_truncated(mat, K = k, symmetric = F, rescale = F, 
-                                           mean_vec = T, sd_vec = T, K_full_rank = F)
+  svd_res <- multiomicCCA:::.svd_truncated(mat, K = k, 
+                                           symmetric = F, 
+                                           rescale = F, 
+                                           mean_vec = T, 
+                                           sd_vec = F, 
+                                           K_full_rank = F)
   dimred <- multiomicCCA:::.mult_mat_vec(svd_res$u, svd_res$d)
   kmeans_res <- stats::kmeans(dimred, centers = k)
   metacell_clustering <- lapply(1:k, function(kk){
