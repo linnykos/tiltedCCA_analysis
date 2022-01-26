d2516c565da8bc7f2394b7eb456de046185a8856
linnykos <klin1234@gmail.com>
2022-01-26 09:40:30 -0500

    Trying with more memory

--- experiment/Writeup14j/Writeup14j_citeseq_pbmc224_dcca_postprocess_clisi.R
+++ experiment/Writeup14j/Writeup14j_citeseq_pbmc224_dcca_postprocess_clisi.R
@@ -12,13 +12,16 @@ rna_frnn <- multiomicCCA::construct_frnn(dcca_res2,
                                          data_1 = T, 
                                          data_2 = F,
                                          normalization_type = "cosine_itself")
+save(rna_frnn, 
+     file = "../../../../out/Writeup14j/Writeup14j_citeseq_pbmc224_clisi.RData")
+
 set.seed(10)
 adt_frnn <- multiomicCCA::construct_frnn(dcca_res2, 
                                          data_1 = F, 
                                          data_2 = T,
                                          normalization_type = "cosine_itself")
 save(rna_frnn, adt_frnn, 
-     file = "../../../../out/Writeup14g/Writeup14g_citeseq_pbmc224_clisi.RData")
+     file = "../../../../out/Writeup14j/Writeup14j_citeseq_pbmc224_clisi.RData")
 
 set.seed(10)
 common_g <- multiomicCCA::combine_frnn(dcca_res2, 
@@ -27,7 +30,7 @@ common_g <- multiomicCCA::combine_frnn(dcca_res2,
                                        nn = 30,
                                        verbose = 1)
 save(rna_frnn, adt_frnn, common_g,
-     file = "../../../../out/Writeup14g/Writeup14g_citeseq_pbmc224_clisi.RData")
+     file = "../../../../out/Writeup14j/Writeup14j_citeseq_pbmc224_clisi.RData")
 
 set.seed(10)
 rna_clisi <- multiomicCCA::clisi_information(common_g, rna_frnn$d_g,
@@ -37,7 +40,7 @@ rna_clisi$common_clisi$clisi_mat
 rna_clisi$distinct_clisi$clisi_mat
 
 save(rna_clisi, 
-     file = "../../../../out/Writeup14g/Writeup14g_citeseq_pbmc224_clisi.RData")
+     file = "../../../../out/Writeup14j/Writeup14j_citeseq_pbmc224_clisi.RData")
 
 set.seed(10)
 adt_clisi <- multiomicCCA::clisi_information(common_g, adt_frnn$d_g,
@@ -47,4 +50,4 @@ adt_clisi$common_clisi$clisi_mat
 adt_clisi$distinct_clisi$clisi_mat
 
 save(rna_clisi, adt_clisi,
-     file = "../../../../out/Writeup14g/Writeup14g_citeseq_pbmc224_clisi.RData")
+     file = "../../../../out/Writeup14j/Writeup14j_citeseq_pbmc224_clisi.RData")
--- experiment/Writeup14j/Writeup14j_citeseq_pbmc224_dcca_postprocess_clisi.sh
+++ experiment/Writeup14j/Writeup14j_citeseq_pbmc224_dcca_postprocess_clisi.sh
@@ -2,6 +2,6 @@
 #$ -N pbmc224_dcca_clisi
 #$ -j y
 #$ -o ../../../../out/Writeup14j/qsub/
-#$ -l m_mem_free=100G
+#$ -l m_mem_free=200G
 
 Rscript --no-save Writeup14j_citeseq_pbmc224_dcca_postprocess_clisi.R
