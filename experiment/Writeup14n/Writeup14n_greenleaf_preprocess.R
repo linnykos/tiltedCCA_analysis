e57bc442884cfe0dfcfaa3b06710e8e3457fbc60
linnykos <klin1234@gmail.com>
2022-04-18 21:54:57 -0400

    Pushing the dataset processing

--- experiment/Writeup14n/Writeup14n_greenleaf_preprocess.R
+++ experiment/Writeup14n/Writeup14n_greenleaf_preprocess.R
@@ -1,14 +1,5 @@
 rm(list=ls())
 
-# load in the older version of the Greenleaf data to 1) determine which cells to keep and 2) transfer its cell metadata over
-# Technically, we could grab these metadata files from $HOME/nzhanglab/data/GSE162170_multiome_cell_metadata.txt.gz as well. Oops!
-load("../../../../out/Writeup14l/Writeup14l_10x_greenleaf_preprocess.RData")
-
-batch_name <- sapply(rownames(greenleaf@meta.data), function(str){
-  tmp <- strsplit(str, split = "_")[[1]]
-  paste0(tmp[1:(length(tmp)-1)], collapse = "_")
-})
-
 library(Seurat); library(Signac)
 
 set.seed(10)
@@ -49,11 +40,9 @@ seurat_list <- lapply(1:length(file_folders), function(i){
   seurat_obj
 })
 
-greenleaf_new <- merge(seurat_list[[1]], y = c(seurat_list[[2]], seurat_list[[3]]), 
-                       add.cell.ids = name_vec, 
-                       project = "hft", merge.data = T)
-table(batch_name)
-table(greenleaf_new$dataset)
+greenleaf <- merge(seurat_list[[1]], y = c(seurat_list[[2]], seurat_list[[3]]), 
+                   add.cell.ids = name_vec, 
+                   project = "hft", merge.data = T)
 
 ###################
 
@@ -108,81 +97,96 @@ greenleaf_atac <- merge(seurat_atac_list[[1]], y = c(seurat_atac_list[[2]], seur
                         add.cell.ids = name_vec, 
                         project = "hft", merge.data = T)
 
-greenleaf_new[["ATAC"]] <- greenleaf_atac[["ATAC"]]
-keep_vec <- rep(0, ncol(greenleaf_new))
-tmp <- colnames(greenleaf_new); tmp <- sapply(tmp, function(x){strsplit(x, split = "-")[[1]][1]}); names(tmp) <- NULL
-keep_vec[which(tmp %in% colnames(greenleaf))] <- 1
-table(keep_vec)
-greenleaf_new$keep <- keep_vec
-greenleaf_new <- subset(greenleaf_new, keep == 1)
+greenleaf[["ATAC"]] <- greenleaf_atac[["ATAC"]]
+##########################
 
-########################################3
+cell_metadata <- readr::read_delim("~/nzhanglab/data/GSE162170_cortical_multiome/GSE162170_multiome_cell_metadata.txt.gz")
+cluster_metadata <- readr::read_delim("~/nzhanglab/data/GSE162170_cortical_multiome/GSE162170_multiome_cluster_names.txt.gz")
+cluster_metadata <- as.data.frame(cluster_metadata)
+cluster_metadata <- cluster_metadata[which(cluster_metadata$Assay == "Multiome RNA"),]
+cell_metadata$Cell.ID <- paste0(cell_metadata$Cell.ID, "-1")
+all(cell_metadata$Cell.ID %in% colnames(greenleaf))
+keep_vec <- rep(0, ncol(greenleaf))
+keep_vec[which(colnames(greenleaf) %in% cell_metadata$Cell.ID)] <- 1
+greenleaf$keep <- keep_vec
+greenleaf <- subset(greenleaf, keep == 1)
+all(cell_metadata$Cell.ID == colnames(greenleaf))
 
-name_vec <- colnames(greenleaf_new)
-name_vec <- sapply(name_vec, function(x){strsplit(x, split = "-")[[1]][1]})
-names(name_vec) <- NULL
-
-age_vec <- rep(NA, ncol(greenleaf_new)) # Sample.Age
-for(i in 1:ncol(greenleaf_new)){
-  age_vec[i] <- greenleaf$Sample.Age[which(rownames(greenleaf@meta.data) == name_vec[i])]
+celltype_vec <- as.character(cell_metadata$seurat_clusters)
+for(i in 1:nrow(cluster_metadata)){
+  idx <- which(celltype_vec == cluster_metadata[i,"Cluster.ID"])
+  celltype_vec[idx] <- cluster_metadata[i,"Cluster.Name"]
 }
-table(age_vec, greenleaf$Sample.Age)
 
-celltype_vec <- rep(NA, ncol(greenleaf_new)) # celltype
-for(i in 1:ncol(greenleaf_new)){
-  celltype_vec[i] <- greenleaf$celltype[which(rownames(greenleaf@meta.data) == name_vec[i])]
-}
-table(celltype_vec, greenleaf$celltype)
-
-batch_vec <- rep(NA, ncol(greenleaf_new)) # Sample.Batch
-for(i in 1:ncol(greenleaf_new)){
-  batch_vec[i] <- greenleaf$Sample.Batch[which(rownames(greenleaf@meta.data) == name_vec[i])]
-}
-table(batch_vec, greenleaf$Sample.Batch)
-
-greenleaf_new$Sample.Age <- age_vec
-greenleaf_new$celltype <- celltype_vec
-greenleaf_new$Sample.Batch <- batch_vec
+greenleaf$celltype <- celltype_vec
+greenleaf$Sample.Age <- dplyr::pull(cell_metadata, "Sample.Age")
+greenleaf$Sample.Batch <- dplyr::pull(cell_metadata, "Sample.Batch")
 
 ###################
 
 set.seed(10)
-Seurat::DefaultAssay(greenleaf_new) <- "RNA"
-greenleaf_new <- Seurat::SCTransform(greenleaf_new)
-greenleaf_new <- Seurat::FindVariableFeatures(greenleaf_new)
-greenleaf_new <- Seurat::RunPCA(greenleaf_new, verbose = FALSE)
+Seurat::DefaultAssay(greenleaf) <- "RNA"
+greenleaf <- Seurat::SCTransform(greenleaf)
+greenleaf <- Seurat::FindVariableFeatures(greenleaf)
+greenleaf <- Seurat::RunPCA(greenleaf, verbose = FALSE)
 set.seed(10)
-greenleaf_new <- Seurat::RunUMAP(greenleaf_new, dims = 1:50)
+greenleaf <- Seurat::RunUMAP(greenleaf, dims = 1:50)
 
 set.seed(10)
-DefaultAssay(greenleaf_new) <- "ATAC"
-greenleaf_new <- Signac::RunTFIDF(greenleaf_new)
-greenleaf_new <-  Signac::FindTopFeatures(greenleaf_new, min.cutoff="q10")
-greenleaf_new <-  Signac::RunSVD(greenleaf_new)  
+DefaultAssay(greenleaf) <- "ATAC"
+greenleaf <- Signac::RunTFIDF(greenleaf)
+greenleaf <-  Signac::FindTopFeatures(greenleaf, min.cutoff="q10")
+greenleaf <-  Signac::RunSVD(greenleaf)  
 set.seed(10)
-greenleaf_new <- Seurat::RunUMAP(greenleaf_new, reduction="lsi", 
+greenleaf <- Seurat::RunUMAP(greenleaf, reduction="lsi", 
                              dims=2:50, reduction.name="umap.atac", 
                              reduction.key="atacUMAP_")
 
 ####################
 
 set.seed(10)
-greenleaf_new <- Seurat::FindMultiModalNeighbors(greenleaf_new, reduction.list = list("pca", "lsi"), 
-                                             dims.list = list(1:50, 2:50))
+greenleaf <- Seurat::FindMultiModalNeighbors(greenleaf, reduction.list = list("pca", "lsi"), 
+                                                 dims.list = list(1:50, 2:50))
 set.seed(10)
-greenleaf_new <- Seurat::RunUMAP(greenleaf_new, nn.name = "weighted.nn", reduction.name = "umap.wnn", 
-                             reduction.key = "wnnUMAP_")
+greenleaf <- Seurat::RunUMAP(greenleaf, nn.name = "weighted.nn", reduction.name = "umap.wnn", 
+                                 reduction.key = "wnnUMAP_")
 
-Seurat::DefaultAssay(greenleaf_new) <- "SCT"
-greenleaf_new[["percent.mt"]] <- Seurat::PercentageFeatureSet(object = greenleaf_new, pattern = "^MT-")
-greenleaf_new[["percent.rb"]] <- Seurat::PercentageFeatureSet(object = greenleaf_new, pattern = "^RPS")
-greenleaf_new <- Seurat::CellCycleScoring(greenleaf_new, 
-                                      g2m.features = cc.genes$g2m.genes, 
-                                      s.features = cc.genes$s.genes)
+Seurat::DefaultAssay(greenleaf) <- "SCT"
+greenleaf[["percent.mt"]] <- Seurat::PercentageFeatureSet(object = greenleaf, pattern = "^MT-")
+greenleaf[["percent.rb"]] <- Seurat::PercentageFeatureSet(object = greenleaf, pattern = "^RPS")
+greenleaf <- Seurat::CellCycleScoring(greenleaf, 
+                                          g2m.features = cc.genes$g2m.genes, 
+                                          s.features = cc.genes$s.genes)
+
+####################
+
+# load in gene activity
+load("~/nzhanglab/data/GSE162170_cortical_multiome/GSE162170_multiome_atac_gene_activities.RData")
+
+name_vec <- colnames(greenleaf)
+name_vec <- sapply(name_vec, function(x){strsplit(x, split = "-")[[1]][1]})
+names(name_vec) <- NULL
+
+mat <- mat[,name_vec]
+colnames(mat) <- colnames(greenleaf)
+rownames(mat) <- paste0("ATAC-", rownames(mat))
+
+greenleaf[["geneActivity"]] <- Seurat::CreateAssayObject(counts = mat)
+Seurat::DefaultAssay(greenleaf) <- "geneActivity"
+greenleaf <- Seurat::NormalizeData(greenleaf)
+greenleaf[["geneActivity"]]@var.features <- paste0("ATAC-", greenleaf[["SCT"]]@var.features)
+greenleaf <- Seurat::ScaleData(greenleaf)
+greenleaf <- Seurat::RunPCA(greenleaf, verbose = FALSE,
+                            reduction.name = "pcaGeneActivity",
+                            reduction.key = "PCAGeneActivity_")
+set.seed(10)
+greenleaf <- Seurat::RunUMAP(greenleaf, reduction="pcaGeneActivity", 
+                             dims=2:50, reduction.name="umap.geneActivity", 
+                             reduction.key = "geneActivityUMAP_")
 
 ###################
 
-plot1 <- Seurat::DimPlot(greenleaf_new, reduction = "umap",
+plot1 <- Seurat::DimPlot(greenleaf, reduction = "umap",
                          group.by = "celltype", label = TRUE,
                          repel = TRUE, label.size = 2.5)
 plot1 <- plot1 + ggplot2::ggtitle(paste0("Human brain (10x, RNA+ATAC)\nRNA UMAP"))
@@ -190,8 +194,7 @@ plot1 <- plot1 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
 ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14n/Writeup14n_greenleaf_rna-umap.png"),
                 plot1, device = "png", width = 6, height = 5, units = "in")
 
-
-plot2 <- Seurat::DimPlot(greenleaf_new, reduction = "umap.atac",
+plot2 <- Seurat::DimPlot(greenleaf, reduction = "umap.atac",
                          group.by = "celltype", label = TRUE,
                          repel = TRUE, label.size = 2.5)
 plot2 <- plot2 + ggplot2::ggtitle(paste0("Human brain (10x, RNA+ATAC)\nATAC UMAP"))
@@ -199,7 +202,7 @@ plot2 <- plot2 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
 ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14n/Writeup14n_greenleaf_atac-umap.png"),
                 plot2, device = "png", width = 6, height = 5, units = "in")
 
-plot3 <- Seurat::DimPlot(greenleaf_new, reduction = "umap.wnn",
+plot3 <- Seurat::DimPlot(greenleaf, reduction = "umap.wnn",
                          group.by = "celltype", label = TRUE,
                          repel = TRUE, label.size = 2.5)
 plot3 <- plot3 + ggplot2::ggtitle(paste0("Human brain (10x, RNA+ATAC)\nWNN UMAP"))
@@ -207,27 +210,19 @@ plot3 <- plot3 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
 ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14n/Writeup14n_greenleaf_wnn-umap.png"),
                 plot3, device = "png", width = 6, height = 5, units = "in")
 
-####################
-
-# load in gene activity
-load("~/nzhanglab/data/GSE162170_cortical_multiome/GSE162170_multiome_atac_gene_activities.RData")
-
-name_vec <- colnames(greenleaf_new)
-name_vec <- sapply(name_vec, function(x){strsplit(x, split = "-")[[1]][1]})
-names(name_vec) <- NULL
-
-mat <- mat[,name_vec]
-colnames(mat) <- colnames(greenleaf_new)
-rownames(mat) <- paste0("ATAC-", rownames(mat))
-
-greenleaf_new[["geneActivity"]] <- Seurat::CreateAssayObject(counts = mat)
+plot4 <- Seurat::DimPlot(greenleaf, reduction = "umap.geneActivity",
+                         group.by = "celltype", label = TRUE,
+                         repel = TRUE, label.size = 2.5)
+plot4 <- plot4 + ggplot2::ggtitle(paste0("Human brain (10x, RNA+ATAC)\nGene Activity UMAP"))
+plot4 <- plot4 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
+ggplot2::ggsave(filename = paste0("../../../../out/figures/Writeup14n/Writeup14n_greenleaf_geneActivity-umap.png"),
+                plot4, device = "png", width = 6, height = 5, units = "in")
 
 ###################
 
 date_of_run <- Sys.time()
 session_info <- devtools::session_info()
 
-greenleaf <- greenleaf_new
 save(greenleaf, date_of_run, session_info,
      file = "../../../../out/Writeup14n/Writeup14n_10x_greenleaf.RData")
 
