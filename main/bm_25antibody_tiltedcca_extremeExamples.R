14641d5bcd4cc53eb379dcd2fa5be188ee94ab7e
linnykos <klin1234@gmail.com>
2022-04-19 17:09:31 -0400

    Removing metacells for 224 antibody

--- main/bm_25antibody_tiltedcca_extremeExamples.R
+++ main/bm_25antibody_tiltedcca_extremeExamples.R
@@ -1,4 +1,53 @@
 rm(list=ls())
 load("../../../out/main/citeseq_bm25_tcca.RData")
+source("bm_25antibody_colorPalette.R")
 
 library(Seurat); library(Signac)
+
+multiSVD_obj2 <- tiltedCCA:::tiltedCCA(input_obj = multiSVD_obj,
+                                       fix_tilt_perc = 0.9,
+                                       verbose = 1)
+multiSVD_obj2 <- tiltedCCA:::tiltedCCA_decomposition(input_obj = multiSVD_obj2,
+                                                     verbose = 1)
+set.seed(10)
+bm[["common_tcca2"]] <- tiltedCCA:::create_SeuratDim(input_obj = multiSVD_obj2,
+                                                    what = "common",
+                                                    aligned_umap_assay = "rna.umap",
+                                                    seurat_obj = bm,
+                                                    seurat_assay = "RNA",
+                                                    verbose = 1)
+
+plot1 <- Seurat::DimPlot(bm, reduction = "common_tcca2",
+                         group.by = "celltype.l2", 
+                         cols = col_palette)
+plot1 <- plot1 + Seurat::NoLegend() + Seurat::NoAxes()
+plot1 <- plot1 + ggplot2::ggtitle("")
+plot1 <- plot1 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
+ggplot2::ggsave(filename = paste0("../../../out/figures/main/citeseq_bm25_extreme_tilt09_tcca-umap_cleaned.png"),
+                plot1, device = "png", width = 3, height = 3, units = "in",
+                dpi = 500)
+
+##########
+
+multiSVD_obj3 <- tiltedCCA:::tiltedCCA(input_obj = multiSVD_obj,
+                                       fix_tilt_perc = 0.1,
+                                       verbose = 1)
+multiSVD_obj3 <- tiltedCCA:::tiltedCCA_decomposition(input_obj = multiSVD_obj3,
+                                                     verbose = 1)
+set.seed(10)
+bm[["common_tcca3"]] <- tiltedCCA:::create_SeuratDim(input_obj = multiSVD_obj3,
+                                                    what = "common",
+                                                    aligned_umap_assay = "rna.umap",
+                                                    seurat_obj = bm,
+                                                    seurat_assay = "RNA",
+                                                    verbose = 1)
+
+plot1 <- Seurat::DimPlot(bm, reduction = "common_tcca3",
+                         group.by = "celltype.l2", 
+                         cols = col_palette)
+plot1 <- plot1 + Seurat::NoLegend() + Seurat::NoAxes()
+plot1 <- plot1 + ggplot2::ggtitle("")
+plot1 <- plot1 + ggplot2::theme(legend.text = ggplot2::element_text(size = 5))
+ggplot2::ggsave(filename = paste0("../../../out/figures/main/citeseq_bm25_extreme_tilt01_tcca-umap_cleaned.png"),
+                plot1, device = "png", width = 3, height = 3, units = "in",
+                dpi = 500)
