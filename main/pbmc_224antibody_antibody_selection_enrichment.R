5317e9a745677dcfbf22d92c84f0dfd76f40676a
linnykos <klin1234@gmail.com>
2022-05-08 22:37:07 -0400

    Modifying enrichment script

--- main/pbmc_224antibody_antibody_selection_enrichment.R
+++ main/pbmc_224antibody_antibody_selection_enrichment.R
@@ -16,46 +16,46 @@ membership_vec <- droplevels(as.factor(pbmc$celltype.l2))
 print("Enrichment selected")
 set.seed(10)
 enrichment_selected <- tiltedCCA:::postprocess_cell_enrichment(input_obj = consensus_pca$dimred_consensus,
-                                                   membership_vec = membership_vec, 
-                                                   num_neigh = multiSVD_obj$param$snn_num_neigh,
-                                                   bool_cosine = multiSVD_obj$param$snn_bool_cosine,
-                                                   bool_intersect = multiSVD_obj$param$snn_bool_intersect,
-                                                   max_subsample = 1000,
-                                                   min_deg = multiSVD_obj$param$snn_min_deg,
-                                                   verbose = 2)
+                                                               membership_vec = membership_vec, 
+                                                               num_neigh = multiSVD_obj$param$snn_num_neigh,
+                                                               bool_cosine = multiSVD_obj$param$snn_bool_cosine,
+                                                               bool_intersect = multiSVD_obj$param$snn_bool_intersect,
+                                                               max_subsample = 5000,
+                                                               min_deg = multiSVD_obj$param$snn_min_deg,
+                                                               verbose = 3)
 
 print("Enrichment alternative 1")
 set.seed(10)
 enrichment_alt_1 <- tiltedCCA:::postprocess_cell_enrichment(input_obj = pbmc_alt[["adt.umap1"]],
-                                                membership_vec = membership_vec, 
-                                                num_neigh = multiSVD_obj$param$snn_num_neigh,
-                                                bool_cosine = multiSVD_obj$param$snn_bool_cosine,
-                                                bool_intersect = multiSVD_obj$param$snn_bool_intersect,
-                                                max_subsample = 1000,
-                                                min_deg = multiSVD_obj$param$snn_min_deg,
-                                                verbose = 2)
+                                                            membership_vec = membership_vec, 
+                                                            num_neigh = multiSVD_obj$param$snn_num_neigh,
+                                                            bool_cosine = multiSVD_obj$param$snn_bool_cosine,
+                                                            bool_intersect = multiSVD_obj$param$snn_bool_intersect,
+                                                            max_subsample = 5000,
+                                                            min_deg = multiSVD_obj$param$snn_min_deg,
+                                                            verbose = 3)
 
 print("Enrichment alternative 2")
 set.seed(10)
 enrichment_alt_2 <- tiltedCCA:::postprocess_cell_enrichment(input_obj = pbmc_alt[["adt.umap2"]],
-                                                membership_vec = membership_vec, 
-                                                num_neigh = multiSVD_obj$param$snn_num_neigh,
-                                                bool_cosine = multiSVD_obj$param$snn_bool_cosine,
-                                                bool_intersect = multiSVD_obj$param$snn_bool_intersect,
-                                                max_subsample = 1000,
-                                                min_deg = multiSVD_obj$param$snn_min_deg,
-                                                verbose = 2)
+                                                            membership_vec = membership_vec, 
+                                                            num_neigh = multiSVD_obj$param$snn_num_neigh,
+                                                            bool_cosine = multiSVD_obj$param$snn_bool_cosine,
+                                                            bool_intersect = multiSVD_obj$param$snn_bool_intersect,
+                                                            max_subsample = 5000,
+                                                            min_deg = multiSVD_obj$param$snn_min_deg,
+                                                            verbose = 3)
 
 print("Enrichment alternative 3")
 set.seed(10)
 enrichment_alt_3 <- tiltedCCA:::postprocess_cell_enrichment(input_obj = pbmc_alt[["adt.umap3"]],
-                                                membership_vec = membership_vec, 
-                                                num_neigh = multiSVD_obj$param$snn_num_neigh,
-                                                bool_cosine = multiSVD_obj$param$snn_bool_cosine,
-                                                bool_intersect = multiSVD_obj$param$snn_bool_intersect,
-                                                max_subsample = 1000,
-                                                min_deg = multiSVD_obj$param$snn_min_deg,
-                                                verbose = 2)
+                                                            membership_vec = membership_vec, 
+                                                            num_neigh = multiSVD_obj$param$snn_num_neigh,
+                                                            bool_cosine = multiSVD_obj$param$snn_bool_cosine,
+                                                            bool_intersect = multiSVD_obj$param$snn_bool_intersect,
+                                                            max_subsample = 5000,
+                                                            min_deg = multiSVD_obj$param$snn_min_deg,
+                                                            verbose = 3)
 
 save(enrichment_selected, enrichment_alt_1,
      enrichment_alt_2, enrichment_alt_3,
