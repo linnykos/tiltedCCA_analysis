5522cc146b3813045ca66dd626a03abfd2ce239d
linnykos <klin1234@gmail.com>
2022-05-18 18:00:24 -0400

    Trying the other way for ABseq

--- main/bm_97antibody_differential.R
+++ main/bm_97antibody_differential.R
@@ -8,20 +8,20 @@ set.seed(10)
 date_of_run <- Sys.time()
 session_info <- devtools::session_info()
 
-ct2 <- as.character(bm$ct)
-ct2[ct2 %in% c("Conventional dendritic cell 1", "Conventional dendritic cell 2")] <- "cDC"
-bm$ct2 <- ct2
-
-tab <- table(ct2)
-keep_vec <- rep(1, ncol(bm))
-keep_vec[which(bm$ct %in% names(tab)[which(tab <= 300)])] <- 0
-bm$keep <- keep_vec
-bm2 <- subset(bm, keep == 1)
+# ct2 <- as.character(bm$ct)
+# ct2[ct2 %in% c("Conventional dendritic cell 1", "Conventional dendritic cell 2")] <- "cDC"
+# bm$ct2 <- ct2
+# 
+# tab <- table(ct2)
+# keep_vec <- rep(1, ncol(bm))
+# keep_vec[which(bm$ct %in% names(tab)[which(tab <= 300)])] <- 0
+# bm$keep <- keep_vec
+# bm2 <- subset(bm, keep == 1)
 
 print("Working on RNA")
-gene_de_list <- tiltedCCA:::differential_expression(seurat_obj = bm2,
+gene_de_list <- tiltedCCA:::differential_expression(seurat_obj = bm,
                                                     assay = "RNA",
-                                                    idents = "ct2",
+                                                    idents = "ct",
                                                     test_use = "MAST",
                                                     slot = "counts")
 save(gene_de_list, bm,
@@ -29,18 +29,18 @@ save(gene_de_list, bm,
      file = "../../../out/main/abseq_bm97_differential.RData")
 
 ########
+# 
+# tab <- table(ct2)
+# keep_vec <- rep(1, ncol(bm))
+# keep_vec[which(bm$ct %in% names(tab)[which(tab <= 50)])] <- 0
+# bm$keep <- keep_vec
+# bm2 <- subset(bm, keep == 1)
 
-tab <- table(ct2)
-keep_vec <- rep(1, ncol(bm))
-keep_vec[which(bm$ct %in% names(tab)[which(tab <= 50)])] <- 0
-bm$keep <- keep_vec
-bm2 <- subset(bm, keep == 1)
-
-bm2[["AB"]]@var.features <- rownames(bm2[["AB"]])
+bm[["AB"]]@var.features <- rownames(bm[["AB"]])
 print("Working on ADT")
-adt_de_list <- tiltedCCA:::differential_expression(seurat_obj = bm2,
+adt_de_list <- tiltedCCA:::differential_expression(seurat_obj = bm,
                                                    assay = "AB",
-                                                   idents = "ct2",
+                                                   idents = "ct",
                                                    test_use = "wilcox",
                                                    slot = "data")
 
--- main/mouseembryo_differential.R
+++ main/mouseembryo_differential.R
@@ -9,13 +9,6 @@ set.seed(10)
 date_of_run <- Sys.time()
 session_info <- devtools::session_info()
 
-n <- ncol(mbrain)
-keep_vec <- rep(0, n)
-keep_vec[which(mbrain$label_Savercat %in% c("Oligodendrocyte", "Radial glia", 
-                                            "Forebrain GABAergic", "Neuroblast", 
-                                            "Glioblast", "Cortical or hippocampal glutamatergic"))] <- 1
-mbrain$keep <- keep_vec
-mbrain <- subset(mbrain, keep == 1)
 
 print("Working on RNA")
 gene_de_list <- tiltedCCA:::differential_expression(seurat_obj = mbrain,
