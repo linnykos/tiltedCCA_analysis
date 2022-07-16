d13a6114d0091a8a0d2bb8696ac3f983f91a64b4
linnykos <klin1234@gmail.com>
2022-07-15 22:20:29 -0400

    Fixing typo in Reik

--- main/reik_tiltedcca.R
+++ main/reik_tiltedcca.R
@@ -41,7 +41,7 @@ multiSVD_obj <- tiltedCCA:::create_multiSVD(mat_1 = mat_1b, mat_2 = mat_2b,
 multiSVD_obj <- tiltedCCA:::form_metacells(input_obj = multiSVD_obj,
                                            large_clustering_1 = NULL, 
                                            large_clustering_2 = NULL, 
-                                           num_metacells = NULL,
+                                           num_metacells = 5000,
                                            verbose = 1)
 multiSVD_obj <- tiltedCCA:::compute_snns(input_obj = multiSVD_obj,
                                          latent_k = 20,
