99c36d8ad9913527e89fcb85666dea5ef71fc444
linnykos <klin1234@gmail.com>
2023-06-14 01:07:32 -0400

    Fixing small typo

--- main/bm_25antibody_tiltedcca_added-noise_0.R
+++ main/bm_25antibody_tiltedcca_added-noise_0.R
@@ -156,6 +156,6 @@ bm[["distinct2_tcca"]] <- tiltedCCA:::create_SeuratDim(input_obj = multiSVD_obj,
                                                        verbose = 1)
 
 save(multiSVD_obj, bm, 
-     percentage, cor_vec,
+     percentage, 
      date_of_run, session_info,
      file = paste0("../../../out/main/citeseq_bm25_tcca_added-noise_", percentage, ".RData"))
