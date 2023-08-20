c9bf90af17149cc8ff2da16f36326c64e2b9d110
linnykos <klin1234@gmail.com>
2023-08-20 15:26:52 -0700

    Adding slides that I make

--- main/abseq_bm_97antibodyRef_tcca_postprocess-other.R
+++ main/abseq_bm_97antibodyRef_tcca_postprocess-other.R
@@ -26,3 +26,23 @@ plot1 <- plot1 + ggplot2::ggtitle("") + ggplot2::ylab("RNA UMAP 2") + ggplot2::x
 ggplot2::ggsave(filename = paste0("../../../out/figures/main/abseq_bm97Ref_rna-umap_slides-simpler.png"),
                 plot1, device = "png", width = 4, height = 4, units = "in")
 
+############################
+
+bm[["consensusUMAP"]]@cell.embeddings <- tiltedCCA:::.rotate_matrix(
+  source_mat = bm[["rna.umap"]]@cell.embeddings,
+  target_mat = bm[["consensusUMAP"]]@cell.embeddings
+)
+
+plot1 <- Seurat::DimPlot(bm, reduction = "consensusUMAP",
+                         group.by = "ct", 
+                         cols = col_palette, pt.size = .25)
+plot1 <- plot1 + Seurat::NoLegend() 
+plot1 <- plot1 + ggplot2::ggtitle("") + ggplot2::ylab("AB UMAP 2") + ggplot2::xlab("AB UMAP 1")
+ggplot2::ggsave(filename = paste0("../../../out/figures/main/abseq_bm97Ref_consensusPCA-umap_slides-simpler.png"),
+                plot1, device = "png", width = 4, height = 4, units = "in")
+
+#############
+
+load("../../../out/main/abseq_bm97Ref_varSelect.RData")
+
+
